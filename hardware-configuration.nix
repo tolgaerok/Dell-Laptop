5fdb8cfd04190f2519ebc970ca978aef4f802017
wvpianoman <130115651+wvpianoman@users.noreply.github.com>
2023-09-02 20:24:06 -0400

    spelling

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -79,10 +79,5 @@
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   networking.useDHCP = lib.mkDefault true;
 
-  #---------------------------------------------------------------------
-  # Latest real-time (RT) version of the Linux kernel.
-  #---------------------------------------------------------------------
-
-  # kernelPackages = pkgs.linuxPackages-rt_latest;
 
 }
