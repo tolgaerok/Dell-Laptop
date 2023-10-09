4af58deb2b1e6f8859152692750286c7d2adc0f9
Brian F <130115651+wvpianoman@users.noreply.github.com>
2023-10-09 07:10:23 -0400

    Working Config

--- machines/Dell-Latitude-E6540/configuration.nix
+++ machines/Dell-Latitude-E6540/configuration.nix
@@ -1,127 +1,69 @@
-# LAPTOP:           HP EliteBook Folio 9470m
-# CPU:              Intel Core i7-3678U x 4 (2.1 GHz )
-# GRAPHICS:         Intel HD Graphics 4000 (32MB Video Memory)
-# MEMORY:           8GB RAM (Upgradable to 16GB) (1600 MHz DDR3 SDRAM)
-# STORAGE:          500GB SSD Drive, Samsung EVO 870
-# DISPLAY:          14-inch display (1366x768 pixels)
-# PORTS:            Smart Card Reader, Headphone/Mic, Ethernet, DisplayPort, VGA, 3 x USB 3.0 Ports, SD/MMC Memory Reader
-# NETWORK:          Wi-Fi: 802.11a/b/g/n (Intel Centrino Advanced-N 6235)
-# Bluetooth:        Bluetooth 4.0+HS
-# CARD SLOTS:       SD/MMC memory reader
-#--------------------------------------------------------------------------------------------------------------------------
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
 
 { config, pkgs, ... }:
 
 {
   imports = [
 
-    ../../core
-    ../../core/gpu/intel/intel-laptop/HP-Folio-9470M/Eilite-Folio-9470M-HD-Intel-4000.nix
-    ../../user/user-profile-pic
-    ../../user/SOS/SOS.nix
-    ../../user/brian/brian.nix
+    ./core/common
+    ./core/system-tweaks/kernel-tweaks/16GB-SYSTEM/16GB-SYSTEM.nix
+    ./core/system-tweaks/storage-tweaks/SSD/SSD-tweak.nix
+    ./gpu/intel/HD-INTEL.nix
     ./hardware-configuration.nix
-    ./boot
-
+    ./user
+    
   ];
 
-  # --------------------------------------------------------------------
-  # Permit Insecure Packages
-  # --------------------------------------------------------------------
-  nixpkgs.config.permittedInsecurePackages =
-    [ "openssl-1.1.1u" "openssl-1.1.1v" "electron-12.2.3" ];
+  # -----------------------------------------------------------------
+  #   Bootloader.
+  # -----------------------------------------------------------------
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "Dell-HP-ProBook"; # Define your hostname.
 
   #---------------------------------------------------------------------
-  # Name of your pc to appear on the Network
-  #---------------------------------------------------------------------
-  networking.hostName = "Dell_Laptop"; # Define your hostname.
+  #   Allow unfree packages
+  #--------------------------------------------------------------------- 
+  nixpkgs.config.allowUnfree = true;
 
   #---------------------------------------------------------------------
-  # Latest kernel
+  #   Enable the OpenSSH daemon.
   #---------------------------------------------------------------------
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  services.openssh.enable = true;
 
-  #---------------------------------------------------------------------
-  # Install a couple of basic, off the bat pkgs
-  #---------------------------------------------------------------------
-  environment.systemPackages = with pkgs; [
-
-    appimage-run
-    espeak-classic
-    firefox
-    kate
-
-    # direnv
-    # nix-direnv
-
-    #-----------------------------------------------------------------
-    # Extra Audio packages
-    #-----------------------------------------------------------------
-    alsa-utils
-    pavucontrol
-    pulseaudio
-    pulsemixer
-
-  ];
-
-  #---------------------------------------------------------------------
-  # Enable networking
-  #---------------------------------------------------------------------
+  # -----------------------------------------------------------------
+  #   Enable networking
+  # -----------------------------------------------------------------
   networking.networkmanager.enable = true;
 
-  networking.networkmanager.connectionConfig = {
-    "ethernet.mtu" = 1462;
-    "wifi.mtu" = 1462;
-  };
-  #---------------------------------------------------------------------
-  # Set your time zone.
-  #---------------------------------------------------------------------
-  time.timeZone = "America/New_York";
-
-  #---------------------------------------------------------------------
-  # Select internationalisation properties.
-  #---------------------------------------------------------------------
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_US.UTF-8";
-    LC_IDENTIFICATION = "en_US.UTF-8";
-    LC_MEASUREMENT = "en_US.UTF-8";
-    LC_MONETARY = "en_US.UTF-8";
-    LC_NAME = "en_US.UTF-8";
-    LC_NUMERIC = "en_US.UTF-8";
-    LC_PAPER = "en_US.UTF-8";
-    LC_TELEPHONE = "en_US.UTF-8";
-    LC_TIME = "en_US.UTF-8";
-  };
-
-  #---------------------------------------------------------------------
-  # Enable the X11 windowing system.
-  #---------------------------------------------------------------------
+  # -----------------------------------------------------------------
+  #   Enable the X11 windowing system.
+  # -----------------------------------------------------------------
   services.xserver.enable = true;
 
-  #---------------------------------------------------------------------
-  # Enable the KDE Plasma Desktop Environment.
-  #---------------------------------------------------------------------
+  # -----------------------------------------------------------------
+  #   Enable the KDE Plasma Desktop Environment.
+  # -----------------------------------------------------------------
   services.xserver.displayManager.sddm.enable = true;
   services.xserver.desktopManager.plasma5.enable = true;
 
   #---------------------------------------------------------------------
-  # Configure keymap in X11
+  # Enable automatic login for the user.
   #---------------------------------------------------------------------
-  services.xserver = {
-    layout = "us";
-    xkbVariant = "";
-  };
+  services.xserver.displayManager.autoLogin.enable = true;
+  services.xserver.displayManager.autoLogin.user = "brian";
 
-  #---------------------------------------------------------------------
-  # Enable CUPS to print documents.
-  #---------------------------------------------------------------------
+  # -----------------------------------------------------------------
+  #   Enable CUPS to print documents.
+  # -----------------------------------------------------------------
   services.printing.enable = true;
 
-  #---------------------------------------------------------------------
-  # Enable sound with pipewire.
-  #---------------------------------------------------------------------
+  # -----------------------------------------------------------------
+  #   Enable sound with pipewire.
+  # -----------------------------------------------------------------
   sound.enable = true;
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
@@ -133,31 +75,20 @@
 
   };
 
-  #---------------------------------------------------------------------
-  # Enable touchpad support (enabled default in most desktopManager).
-  #---------------------------------------------------------------------
+  # -----------------------------------------------------------------
+  #   Enable touchpad support (enabled default in most desktopManager).
+  # -----------------------------------------------------------------
   services.xserver.libinput.enable = true;
 
-  #---------------------------------------------------------------------
-  # Enable automatic login for the user.
-  #---------------------------------------------------------------------
-  services.xserver.displayManager.autoLogin.enable = true;
-  services.xserver.displayManager.autoLogin.user = "brian";
+  environment.systemPackages = with pkgs;
+    [
+      #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+      #  wget
+    ];
 
   #---------------------------------------------------------------------
-  # Allow unfree packages
-  #---------------------------------------------------------------------
-  environment.sessionVariables.NIXPKGS_ALLOW_UNFREE = "1";
-  nixpkgs.config.allowUnfree = true;
-
-  #---------------------------------------------------------------------
-  # Enable the OpenSSH daemon.
-  #---------------------------------------------------------------------
-  services.openssh.enable = true;
-
-  #---------------------------------------------------------------------
-  # Automatic system upgrades, automatically reboot after an upgrade if
-  # necessary
+  #   Automatic system upgrades, automatically reboot after an upgrade if
+  #   necessary
   #---------------------------------------------------------------------
   # system.autoUpgrade.allowReboot = true;  # Very annoying .
   system.autoUpgrade.enable = true;
--- user/brian/sshKeys.nix
+++
@@ -1,5 +0,0 @@
-{
-  brian = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGB6OJZcMPd0DQqQTE/H+Frj9EQVMM63Ai2WFfq5kEtl dbfrancisco@gmail.com"
-  ];
-}
